373885769ea0ff57e253dc033d6fe90d44079d9a
ShakieVan <55868847+ShakieVan@users.noreply.github.com>
2026-08-30 14:29:12 +0200

    Release Bill Check v0.3.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "de.shakie.billcheck"
         minSdk = 36
         targetSdk = 36
-        versionCode = 3
-        versionName = "0.3.0"
+        versionCode = 4
+        versionName = "0.3.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
